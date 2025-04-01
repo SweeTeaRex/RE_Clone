d4d64a912aae72fef3234c8b6aeebd0493d875cf
Trey <trey.m.adams92@gmail.com>
2025-04-01 17:38:40 -0500

    Update

--- game.c
+++ game.c
@@ -18,6 +18,11 @@
 #include <string.h>
 #include <raylib.h>
 
+typedef enum GameScreen {
+    TITLE=0,
+    LEVEL1
+} GameScreen;
+
 
 int main(void)
 {
@@ -26,13 +31,54 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "RECLONE");
 
+    GameScreen currentScreen = TITLE;
+
 
     while(!WindowShouldClose())
-    {
+    {   
+        if(IsGamepadAvailable(0))
+        {
+        switch (currentScreen)
+        {
+            case TITLE:
+            {
+                if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_MIDDLE_RIGHT))
+                {
+                    currentScreen = LEVEL1;
+                }
+            
+            } break;
 
+            case LEVEL1:
+            {
+
+            } break;
+        }
+        }
+        
         BeginDrawing();
 
-        ClearBackground(WHITE);
+            ClearBackground(WHITE);
+
+            switch(currentScreen)
+            {
+                case TITLE:
+                {
+                    int fontSize = 40;
+                    
+                    int startText = MeasureText("PRESS START", fontSize);
+                    int reText = MeasureText("RESIDENT EVIL CLONE", fontSize);
+                    DrawText("RESIDENT EVIL CLONE", screenWidth/2-(reText/2), 20, fontSize, RED);
+
+
+                    DrawText("PRESS START", screenWidth/2-(startText/2), screenHeight-60, fontSize, RED);
+
+                } break;
+                case LEVEL1:
+                {
+
+                } break;
+            }
 
 
 
